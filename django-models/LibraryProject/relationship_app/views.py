781ac343955cdcea4784910e286b65982d618ac1
root <root@EsToDa.localdomain>
2024-08-17 19:16:03 +0300

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,9 @@
 from django.shortcuts import render
 from .models import Book, Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 def book_list_view(request):
     books = Book.objects.all()  # Query to get all books
@@ -25,3 +28,8 @@ class LibraryDetailView(DetailView):
         books = library.books.all()
         context['books'] = books
         return context
+    
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy
+    template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/register.html'
